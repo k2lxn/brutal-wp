fb74fb1bc0be756dc9f66b82721d9768fae5a996
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-02-25 22:34:12 -0500

    Only display post excerpts on posts home page

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,29 +11,33 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
+		<?php 
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+		?>
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php brutal_posted_on(); ?>
-			<?php //brutal_byline(); ?> 
-		</div><!-- .entry-meta -->
 		<?php
+		if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php brutal_posted_on(); ?>
+				<?php //brutal_byline(); ?> 
+			</div><!-- .entry-meta -->
+		<?php 
 		endif; ?>
+	
 	</header><!-- .entry-header -->
 
 	<?php brutal_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
+		/*	
 			the_content( sprintf(
 				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
+					// translators: %s: Name of current post. Only visible to screen readers 
 					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'brutal' ),
 					array(
 						'span' => array(
@@ -43,7 +47,27 @@
 				),
 				get_the_title()
 			) );
+		*/		
 
+			
+			$continue_reading_text = sprintf( wp_kses(
+					 //translators: %s: Name of current post. Only visible to screen readers 
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'brutal' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			); 
+			
+			if ( is_singular() ) :
+				the_content( $continue_reading_text );
+			else :
+				the_excerpt( $continue_reading_text );
+			endif;
+		
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'brutal' ),
 				'after'  => '</div>',
@@ -55,3 +79,5 @@
 		<?php brutal_tag_list(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
+
+
