564e6adc9762b7f68a87fc3a85e3f08a0ab3e58c
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-03-04 17:40:19 -0500

    Print year and primary site author (assumed id=1) in footer

--- footer_default.php
+++ footer_default.php
@@ -15,16 +15,15 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<span>&copy; <?php echo date('Y'); ?></span>
-
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'brutal' ) ); ?>"><?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'brutal' ), 'WordPress' );
+			?></a>
 			<span class="sep"> | </span>
-			<span>
 			<?php
-				$author_string = get_the_author_meta('display_name', 1);
-				/* translators: 1: Primary site author. */
-				printf( esc_html__( '%1$s', 'brutal' ), $author_string );
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'brutal' ), 'brutal', '<a href="http://janereynolds.rocks">Jane Reynolds</a>' );
 			?>
-			</span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
