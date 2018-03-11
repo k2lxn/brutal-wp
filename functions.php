451a4e68449ba387f69c2f3bc3ad371f3382733f
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-03-11 12:29:35 -0400

    Full bleed inline images on small screens

--- functions.php
+++ functions.php
@@ -147,6 +147,7 @@ function brutal_scripts() {
 		'collapse' => __( 'Collapse child menu', 'brutal' )
 	));
 	
+	wp_enqueue_script( 'brutal-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20180311', true );
 
 	wp_enqueue_script( 'brutal-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
