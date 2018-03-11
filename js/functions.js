cdbc262012191f07684071ef6b211a1e948a43db
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-03-11 13:23:19 -0400

    Add function to size caption for images that are responsively fullbleed on small screens only

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,23 @@
 ( function($){
 	$('figure.wp-caption.aligncenter').removeAttr('style');
 	$('img.aligncenter').wrap('<figure class="centered-image"/>');
-} )(jQuery)
+
+	// if figure.centered-image has caption, give caption width of image
+	function sizeCenteredCaption( elem ) {
+		var target_width = $( elem ).siblings('img').width();
+		if ( target_width < window.innerWidth ) {
+			$( elem ).width( target_width );
+		}
+	}
+
+	$('figure.aligncenter .wp-caption-text').each( function(){ 
+		sizeCenteredCaption( $(this) ); 
+	});
+	$(window).resize( function() {
+		$('figure.aligncenter .wp-caption-text').each( function(){ 
+			sizeCenteredCaption( $(this) ); 
+		});
+	});
+
+} )(jQuery);
+
