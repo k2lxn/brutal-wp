b5db4aebc52acb467a680ffbaa1aa33a85178438
Jane Reynolds <jane.e.reynolds@gmail.com>
2018-05-28 22:09:12 -0400

    Add #front-page wrapper in front page header

--- header-front.php
+++ header-front.php
@@ -21,7 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="front-page" class="site">
 	<!--<a class="skip-link screen-reader-text" href="#content"><?php #esc_html_e( 'Skip to content', 'brutal' ); ?></a>-->
 
 	<!--<header id="masthead" class="site-header"></header>--><!-- #masthead -->
